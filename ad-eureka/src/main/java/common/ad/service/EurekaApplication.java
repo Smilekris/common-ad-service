77daf9cc8e0d4d44d96bcaaafaf86161d74b645d
chenjiawei <chenjiawei@ecpark.cn>
2020-07-15 19:27:28 +0800

    添加EUREKA

---
+++ ad-eureka/src/main/java/common/ad/service/EurekaApplication.java
@@ -0,0 +1,18 @@
+package common.ad.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaApplication
+ * @Author kris
+ * @Date 2020/7/15
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
